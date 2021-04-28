7685d0e5301825ad30a19e7b58d40b5cb97f6ca6
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2021-04-29 03:52:45 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,151 @@
+
+
+from __future__ import division
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+#from OpenGL.GLU import *
+
+from sys import exit as exitsystem
+
+from numpy import array, count_nonzero
+
+from shaders import *
+
+WIDTH, HEIGHT = 1920, 1080
+
+zoomOut = 1.001
+zoomIn = 0.999
+movementSpeed = 0.002
+
+class Main(object):
+    def __init__(self):
+        pygame.init()
+        self.resolution = WIDTH, HEIGHT
+        pygame.display.set_mode(self.resolution, DOUBLEBUF | OPENGL)
+        pygame.display.set_caption('PyShadeToy')
+
+        # Shaders
+        self.vertex_shader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
+        self.fragment_shader = shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER)
+
+        # Shader program which hosts the vertex and fragment shader
+        self.shader = shaders.compileProgram(self.vertex_shader, self.fragment_shader)
+
+        # Get the uniform locations
+        self.uni_mouse = glGetUniformLocation(self.shader, 'iMouse')
+        self.uni_ticks = glGetUniformLocation(self.shader, 'iTime')
+
+        glUseProgram(self.shader)   # Need to be enabled before sending uniform variables
+        # Resolution doesn't change. Send it once
+        glUniform2f(glGetUniformLocation(self.shader, 'iResolution'), *self.resolution)
+
+        # Create the fullscreen quad for drawing
+        self.vertices = array([-1.0, -1.0, 0.0,
+                                1.0, -1.0, 0.0,
+                                1.0,  1.0, 0.0,
+                                -1.0, 1.0, 0.0
+                               ], dtype='float32')
+
+        # Generate VAO
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+
+        # Generate VBO which is stored in the VAO state
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices, GL_STATIC_DRAW)
+
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+
+
+        self.clock = pygame.time.Clock()
+        self.CenterX = glGetUniformLocation(self.shader, 'CenterX')
+        self.CenterY = glGetUniformLocation(self.shader, 'CenterY')
+        self.ZoomScale = glGetUniformLocation(self.shader, 'ZoomScale')
+        self.ColorRanges = glGetUniformLocation(self.shader, "ColorRanges")
+    def mainloop(self):
+
+        x, y, z = 0.0, 0.0, 1.0
+
+        #px_data =[WIDTH * HEIGHT, 0.1]
+
+        cr = (0.0001, 0.33333, 0.66667, 1.00)
+
+        while 1:
+            delta = self.clock.tick(8192)
+
+            glClearColor(0.0, 0.0, 0.0, 1.0)
+            glClear(GL_COLOR_BUFFER_BIT)
+
+            #Handle Inputs
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                y =  y + movementSpeed * z
+                if y > 1.0:
+                     y = 1.0
+
+            if keys[pygame.K_DOWN]:
+                y =  y - movementSpeed * z
+                if y < -1.0:
+                    y  = -1.0
+
+            if keys[pygame.K_LEFT]:
+                x = x - movementSpeed * z
+                if x < -1.0:
+                    x  = -1.0
+
+
+            if keys[pygame.K_RIGHT]:
+                x = x + movementSpeed * z
+                if x > 1.0:
+                    x  = 1.0
+
+            if keys[pygame.K_KP_PLUS]:
+                z = z * zoomIn
+                if z > 1.0:
+                    z = 1.0
+
+            if keys[pygame.K_KP_MINUS]:
+                z = z * zoomOut
+                if z > 1.0:
+                    z = 1.0
+
+            for event in pygame.event.get():
+                if (event.type == QUIT) or (event.type == KEYUP and event.key == K_ESCAPE):
+                    pygame.quit()
+                    exitsystem()
+
+                # if event.type == KEYDOWN:
+                #
+                #     print(f'{x}, {y}, zoom: {z}')
+            glUseProgram(self.shader)
+
+            # Send uniform values
+            glUniform2f(self.uni_mouse, *pygame.mouse.get_pos())
+            glUniform1f(self.uni_ticks, pygame.time.get_ticks() / 1000.0)
+            glUniform1f(self.CenterX, x)
+            glUniform1f(self.CenterY, y)
+            glUniform1f(self.ZoomScale, z)
+            glUniform4f(self.ColorRanges, *cr)
+
+            # Bind the vao (which stores the VBO with all the vertices)
+            glBindVertexArray(self.vao)
+            glDrawArrays(GL_QUADS, 0, 4)
+
+            #data = glReadPixels(0, 0 , WIDTH , HEIGHT , GL_DEPTH_COMPONENT, GL_FLOAT)
+            #data = array(data).flatten()
+            #
+            # print(len(data))
+            # print(len(data[0]))
+            #cr = ComputeColor(data)
+
+            pygame.display.set_caption("FPS: {}".format(self.clock.get_fps()))
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    Main().mainloop()
---
+++ shaders.py
@@ -0,0 +1,98 @@
+VERTEX_SHADER = """
+#version 330 core
+layout(location = 0) in vec3 vPos;
+
+void main()
+{
+    gl_Position = vec4( vPos.xyz, 1.0);
+}
+"""
+
+FRAGMENT_SHADER = """
+#version 330 core
+#define MAX_ITERATIONS 500
+
+uniform vec2  iMouse;
+uniform float iTime;
+uniform vec2  iResolution;
+
+uniform float CenterX;
+uniform float CenterY;
+uniform float ZoomScale;
+uniform vec4 ColorRanges;
+
+in vec4 gl_FragCoord;
+out vec4 frag_color;
+out float frag_depth;
+
+int GetIterations()
+{
+    float real = ((gl_FragCoord.x / 1080.0 - 1.0f) * ZoomScale + CenterX )* 4.0;
+    float imag = ((gl_FragCoord.y / 1080.0 - 0.5f) * ZoomScale + CenterY )* 4.0;
+
+    int iterations = 0;
+    float real_number = real;
+    float imaginary = imag;
+
+    while (iterations < MAX_ITERATIONS)
+    {
+        float tmp_real = real;
+        real = (pow(real, 2) - pow(imag, 2)) + real_number;
+        imag = (2.0 * tmp_real * imag) + imaginary;
+
+        float dist = pow(real, 2) + pow(imag, 2);
+
+        if (dist > 4.0){
+            break;
+        }
+
+        ++iterations;
+    }
+    return iterations;
+}
+
+vec4 GetColorValues()
+{
+    vec2 uv = gl_FragCoord.xy/iResolution.xy * 2.0 - 1.0;
+    uv.x *= iResolution.x / iResolution.y;
+
+    int iter = GetIterations();
+    if (iter == MAX_ITERATIONS)
+    {
+        gl_FragDepth = 0.0f;
+        return vec4(0.001f, 0.00f, 0.001f, 1.0f);
+    }
+
+    float iterations = float(iter) / MAX_ITERATIONS;
+    gl_FragDepth = iterations;
+
+    vec3 iColor = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0.0, 2.0, 4.0));
+    vec4 color_0 = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    vec4 color_1 = vec4(iColor.x/2, 0.5f, 0.6f, 1.0f);
+    vec4 color_2 = vec4(0.3f, iColor.y, 0.4f, 1.0f);
+    vec4 color_3 = vec4(iColor, 1.0f);
+
+    float fraction = 0.0f;
+    if (iterations < ColorRanges[1])
+    {
+        fraction = (iterations - ColorRanges[0]) / (ColorRanges[1] - ColorRanges[0]);
+        return mix(color_0, color_1, fraction);
+    }
+    else if(iterations < ColorRanges[2])
+    {
+        fraction = (iterations - ColorRanges[1]) / (ColorRanges[2] - ColorRanges[1]);
+        return mix(color_1, color_2, fraction);
+    }
+    else
+    {
+        fraction = (iterations - ColorRanges[2]) / (ColorRanges[3] - ColorRanges[2]);
+        return mix(color_2, color_3, fraction);
+    }
+}
+
+void main()
+{
+    vec4 color = GetColorValues();
+    frag_color = color;
+}
+"""
